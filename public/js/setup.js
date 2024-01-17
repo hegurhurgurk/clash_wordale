33b046ef473d909034321dff3b121347c032f7e5
Cameron Greene <cam.greene@gmail.com>
2024-01-16 17:28:06 -0800

    Big Push

--- public/js/setup.js
+++ public/js/setup.js
@@ -11,9 +11,9 @@ document.getElementById("game-form").addEventListener("submit", async (e) => {
     e.preventDefault();
 
     let inputElement = document.getElementById("guess-input");
-
+    let userGuess = inputElement.value;
     let data = {
-        "guess": inputElement.value,
+        "guess": userGuess,
     }
     inputElement.value = "";
     const response = await fetch("/guess", {
@@ -23,10 +23,10 @@ document.getElementById("game-form").addEventListener("submit", async (e) => {
         },
         body: JSON.stringify(data)
     });
-    
+
     let ans = await response.json();
     console.log(ans);
-    buildGuessRow(ans);
+    buildGuessRow(userGuess, ans);
     if(ans[0]==true){
         document.getElementById("guess-input").setAttribute("placeholder","Correct!")
     }
@@ -35,7 +35,7 @@ document.getElementById("game-form").addEventListener("submit", async (e) => {
     }
     let gameState = parseInt(localStorage.getItem("gameState"));
     localStorage.setItem("gameState", (gameState + 1).toString());
-    
+
 })
 document.getElementById("randomize").addEventListener("click", async (e) => {
     document.getElementById("game-container").innerHTML='';
@@ -48,8 +48,8 @@ document.getElementById("randomize").addEventListener("click", async (e) => {
             "Content-Type": "application/json"
         },
     });
-    
-    
+
+
 })
 document.getElementById("daily").addEventListener("click", async (e) => {
     document.getElementById("game-container").innerHTML='';
@@ -62,8 +62,8 @@ document.getElementById("daily").addEventListener("click", async (e) => {
             "Content-Type": "application/json"
         },
     });
-    
-    
+
+
 })
 
 // store the current gameplay day in the user's browser
@@ -84,12 +84,22 @@ inputElement.addEventListener('input', (e) => {
 });
 
 function buildAutoComplete() {
-    
+
 }
 
-function buildGuessRow(guess) {
+function buildGuessRow(input, guess) {
     let myRow = document.createElement('ul')
     myRow.className = "row";
+
+    let cardIcon = document.createElement('img');
+    cardIcon.className = "card-image";
+    let formattedCardName = input.toLowerCase();
+    formattedCardName = formattedCardName.replaceAll(/\s+/g,"-");
+
+    let url = "https://cdn.royaleapi.com/static/img/cards-150/".concat(formattedCardName).concat(".png");
+    cardIcon.src = url;
+
+    myRow.appendChild(cardIcon);
 
     for(let i = 1; i < 7; i++) {
         let thisSquare = document.createElement('li');
@@ -105,14 +115,28 @@ function buildGuessRow(guess) {
         }
         if(guess[i]===-1){
             thisSquare.className='red'
-            thisSquare.appendChild(document.createTextNode("More"))
+            thisSquare.appendChild(buildUpArrowIconElement());
         }
         if(guess[i]===1){
-            thisSquare.className='red'
-            thisSquare.appendChild(document.createTextNode("Less"))
+            thisSquare.className='red';
+            thisSquare.appendChild(buildDownArrowIconElement());
         }
-        
+
     }
 
     document.getElementById("game-container").appendChild(myRow);
+}
+
+function buildUpArrowIconElement() {
+    let icon = document.createElement('img');
+    icon.className = "icon";
+    icon.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABX0lEQVR4nO2XMW6DMBhGPwspR2jvgrhH1YWFwgBCHVrlCO150iPAPWBpOnSCoUoW7MrUSBnaBnDwb1o/yQpJJPu9/CgSgONvcgvgHQBXS17fYCU8ABByeZ7Xr+G9+s5q7gfZMAxF27aiaZr+Wn7GGJOvj7CUTN0uIooi0XWdGOCcizRNrY7IfpJfQ0R2Tt7miGysvI0R+VT5XyK2xuUZY7Pkv4vA19quRp4yIr+UPEVEfml5kxGLyZuIWFx+yYhk2CxJkv6ApeGc92ed/MXe6QR8yI3iODYiPyDPkmeqH086zKbfpK5rYZqqqk5vJb2AcysIgsmCvu+P2ls34DD2kKlMkD/oBOwsCNjpBFwDeAFwJAg4KvkrLIxuADkugBo3AWrcBKhxE6DGTYAaNwFq3ASocROwYgJlWY5+mCmKwr4HGsxf5LxpyO9hAc8aAU+wgI2K2E8Qf1XyG2p5x7/nE+AwPrdwDPHzAAAAAElFTkSuQmCC";
+    return icon;
+}
+
+function buildDownArrowIconElement() {
+    let icon = document.createElement('img');
+    icon.className = "icon";
+    icon.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABQElEQVR4nO2YzWqEMBRGP9+nTJftQjftc7hRdOHjdZ6hLfQhdNkf0M3QTcGbkuCAlKaoMbmZcg8EBCE5hzuLiYAgsPMEQG1cj4gA5bjYMSJrkYAdkQlwIxPgRibAjUyAG5kANzIBbmQC3MgEuJEJ+OQA4GPJ5dzhTqz+WO8ArlwC3pYclGXZ6oA0TZdGvLoEmE26rlOhadt2ly8XZoOqqhQRBZMnInPmHgFf503qug4SQUTmLH1mkiRqctjMy/z3WJalGsfRq3zTNHN5vZ5dAm4BfIaIoN/l9dk3cCQDcPIZQXb5e1d57xEUQN5bBAWUt0YURbEpgkN+twhOeeeIGOQ3R8QkvzoiRvnFETHLWyPyPFfDMKi+781zzPLWCC08k45a3hpxSfJn7n7+AZzJ63cXwTWAI4B+Wg/T/Vr4d3wDfyc9KaOsuJkAAAAASUVORK5CYII=";
+    return icon;
 }
